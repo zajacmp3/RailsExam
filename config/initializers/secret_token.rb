bae6fae6d174e804c92404619a8286a7f0ea1c34
zajacmp3 <deejay.zajac@gmail.com>
2014-04-21 16:59:41 +0200

    Dodanie wygenerowanego projektu.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsExam::Application.config.secret_key_base = 'f8343ced0bf8249e121ad85fc780ca81a3af74dde7029e5b7bbded8890c3f237a2315b7c89e975a4acef2b0627413b63fc8439ae6e601e71b6258f272f9c182d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsExam::Application.config.session_store :cookie_store, key: '_RailsExam_session'
